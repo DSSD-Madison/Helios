81d049062d76a8d326476e7545383bce03e4948b
Max Maeder <max.s.maeder@gmail.com>
2023-02-15 19:16:47 -0600

    Api keys

---
+++ frontend/helios-dashboard/src/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAaZSAsa9-7O_ffxqPPVbUsMZ13eDXH7TU",
+  authDomain: "helios-9d435.firebaseapp.com",
+  projectId: "helios-9d435",
+  storageBucket: "helios-9d435.appspot.com",
+  messagingSenderId: "768984637940",
+  appId: "1:768984637940:web:b5a0fe060df09188bdd9a1",
+  measurementId: "G-G6CB757SYF",
+};
+
+export { firebaseConfig };
