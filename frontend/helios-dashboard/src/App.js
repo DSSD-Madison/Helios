c263a24e2ec48943e567a004cb06457a0c286843
Max Maeder <max.s.maeder@gmail.com>
2023-02-20 18:16:57 -0600

    Added panel edit interface

--- frontend/helios-dashboard/src/App.js
+++ frontend/helios-dashboard/src/App.js
@@ -7,28 +7,33 @@ import {
 
 import Admin from "./routes/Admin";
 import AdminGuard from "./guards/AdminGuard";
+import { ConfirmProvider } from "material-ui-confirm";
 import Dashboard from "./routes/Dashboard";
 import Login from "./routes/Login";
+import Panel from "./routes/Panel";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<AdminGuard expectedAuth={false} />}>
-          {/* React-router dropped support for Regex */}
-          <Route path="/auth/log-in" element={<Login />} />
-          <Route path="/auth/create-account" element={<Login />} />
-          <Route path="/auth/reset-password" element={<Login />} />
-          <Route path="/auth" element={<Navigate to="/auth/login" />} />
-        </Route>
+    <ConfirmProvider>
+      <Router>
+        <Routes>
+          <Route element={<AdminGuard expectedAuth={false} />}>
+            {/* React-router dropped support for Regex */}
+            <Route path="/auth/log-in" element={<Login />} />
+            <Route path="/auth/create-account" element={<Login />} />
+            <Route path="/auth/reset-password" element={<Login />} />
+            <Route path="/auth" element={<Navigate to="/auth/login" />} />
+          </Route>
 
-        <Route element={<AdminGuard expectedAuth={true} />}>
-          <Route path="/admin" element={<Admin />} />
-        </Route>
+          <Route element={<AdminGuard expectedAuth={true} />}>
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin/panel/:panelId" element={<Panel />} />
+          </Route>
 
-        <Route path="/" element={<Dashboard />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </ConfirmProvider>
   );
 }
