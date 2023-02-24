39d0037d21b0ff9b1aec0ec8f026b07fd020d325
Jack Blake <jhblake2@wisc.edu>
2023-02-24 14:55:45 -0600

    config dev env and add deployment npm scripts

--- frontend/helios-dashboard/src/firebase.js
+++ frontend/helios-dashboard/src/firebase.js
@@ -1,12 +1,20 @@
 import { firebaseConfig } from "./config";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getStorage, connectStorageEmulator } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const store = getStorage(app);
+
+// process.env.NODE_ENV automatically defined by create-react-app: 
+// https://create-react-app.dev/docs/adding-custom-environment-variables
+if (process.env.NODE_ENV !== "production") {
+    connectAuthEmulator(auth, 'http://localhost:9099');
+    connectFirestoreEmulator(db, 'localhost', 8081);
+    connectStorageEmulator(store, 'localhost', 9199);
+}
 
 export { auth, db, store };
