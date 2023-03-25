ef752bf395b32741237a3d2b3163128bad6e513e
Sarthak Agrawal <sarthavader@gmail.com>
2023-03-25 18:52:11 -0500

    allows list of dates rather than start and end

--- frontend/helios-dashboard/src/weatherData.js
+++ frontend/helios-dashboard/src/weatherData.js
@@ -12,15 +12,58 @@ async function fetchWithTimeout(resource, options = {}) {
 
 }
 
-function fetchPrecipData(startDate, endDate) {
+/**
+ * fetches precipitation data
+ * @param {Date[]} dates 
+ * @returns 
+ */
+function fetchPrecipData(dates) {
+    let early = dates[0]
+    let late = dates[0]
+    let storage = {}
+
+    function convertDateToString(date) {
+        return `${date.getUTCFullYear()}-${(date.getUTCMonth() + 1).toString().padStart(2, '0')}-${date.getUTCDate().toString().padStart(2, '0')}`
+    }
+
+    for (let i = 0; i < dates.length; i++) {
+        if (dates[i].getTime() < early.getTime()) {
+            early = dates[i]
+        }
+
+        if (dates[i].getTime() > late.getTime()) {
+            late = dates[i]
+        }
+
+        // console.log(dates[i])
+        // console.log(convertDateToString(dates[i]))
+
+        storage[convertDateToString(dates[i])] = 0;
+    }
+
+    let fEarly = convertDateToString(early)
+    let fLate = convertDateToString(late)
+    // console.log(`${fEarly}, ${fLate}`)
+
     return new Promise((resolve, reject) => {
-        const url = 'https://archive-api.open-meteo.com/v1/archive?latitude=43.07&longitude=-89.40&start_date=' + startDate + '&end_date=' + endDate + '&daily=precipitation_sum&timezone=America%2FChicago'
+        const url = 'https://archive-api.open-meteo.com/v1/archive?latitude=43.07&longitude=-89.40&start_date=' + fEarly + '&end_date=' + fLate + '&daily=precipitation_sum&timezone=America%2FChicago'
 
         const res = fetchWithTimeout(url);
         res.then(response => {
-            data = response.json()
-            data.then(vals => {
-                resolve(vals['daily']['precipitation_sum'])
+            let data = response.json()
+            data.then(resp => {
+                let retval = []
+                let vals = resp['daily']['precipitation_sum']
+                let curr = early
+                for (let i = 0; i < vals.length; i++) {
+                    let dateString = convertDateToString(curr)
+                    if (dateString in storage) {
+                        retval.push(vals[i] / 10)
+                    }
+                    curr = new Date(curr.getTime() + 1000 * 60 * 60 * 24)
+                }
+
+                resolve(retval)
             })
         })
 
@@ -31,8 +74,9 @@ function fetchPrecipData(startDate, endDate) {
     });
 }
 
-let precipData = fetchPrecipData('2023-02-14', '2023-03-15')
+// console.log(new Date('2023-03-15'))
+// let precipData = fetchPrecipData([new Date('2023-02-15'), new Date('2023-03-15')])
 
-precipData.then((value) => {
-    console.log(value);
-});
+// precipData.then((value) => {
+//     console.log(value);
+// });
