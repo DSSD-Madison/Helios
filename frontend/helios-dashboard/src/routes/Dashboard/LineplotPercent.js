4f4a4e1353d9d43a89ea9b3c099075ed188b8d36
Rise <rise.nattarach@gmail.com>
2023-04-18 12:25:53 -0500

    update to omit the date with nan irradiance from the percent efficiency plot

--- frontend/helios-dashboard/src/routes/Dashboard/LineplotPercent.js
+++ frontend/helios-dashboard/src/routes/Dashboard/LineplotPercent.js
@@ -3,6 +3,7 @@ import Plotly from "plotly.js-dist";
 export function outputIrradiancePercent(data, selectedId) {
   const containerId = "plot-container-percent";
   const traces = [];
+  const datesWithNaN = new Set();
   let latestDate = new Date(0);
 
   for (const [id, arrayData] of Object.entries(data)) {
@@ -16,27 +17,36 @@ export function outputIrradiancePercent(data, selectedId) {
 
     if (!selectedId || (selectedId && id === selectedId)) {
       const arrayName = arrayData.name || `Array ${id}`;
+      const filteredDates = [];
+      const filteredOutput = [];
+      const filteredEfficiency = [];
+
+      // Filter the data and store dates with NaN irradiance values
+      arrayData.dates.forEach((date, i) => {
+        if (isNaN(arrayData.irradiance[i])) {
+          // If irradiance value is NaN, store the date in datesWithNaN
+          datesWithNaN.add(new Date(date).toISOString());
+        } else {
+          // If irradiance value is valid, add the data to the filtered arrays
+          filteredDates.push(date);
+          filteredOutput.push(arrayData.output[i]);
+          filteredEfficiency.push(
+            (arrayData.output[i] / arrayData.irradiance[i]) * 100
+          );
+        }
+      });
+      console.log(datesWithNaN);
       traces.push({
-        x: arrayData.dates,
-        y: arrayData.output.map(
-          (output, i) => (output / arrayData.irradiance[i]) * 100
-        ),
+        x: filteredDates,
+        y: filteredEfficiency,
         mode: "lines",
         name: `${arrayName} Efficiency`,
-        // line: {
-        //   shape: "spline",
-        //   smoothing: 1,
-        // },
       });
       traces.push({
-        x: arrayData.dates,
-        y: Array(arrayData.dates.length).fill(14),
+        x: filteredDates,
+        y: Array(filteredDates.length).fill(14),
         mode: "lines",
         name: `Typical % Efficiency`,
-        // line: {
-        //   shape: "spline",
-        //   smoothing: 1,
-        // },
       });
     }
   }
@@ -62,7 +72,7 @@ export function outputIrradiancePercent(data, selectedId) {
     yaxis: {
       title: "Efficiency (%)",
       range: [0, 40],
-      fixedrange: true
+      fixedrange: true,
     },
     title: {
       text: "Solar Array Efficiency",
@@ -74,14 +84,22 @@ export function outputIrradiancePercent(data, selectedId) {
       r: 10,
       b: 80,
     },
-    dragmode: 'pan'
+    dragmode: "pan",
   };
 
   Plotly.newPlot(containerId, traces, layout, {
     responsive: true,
     displaylogo: false,
-    modeBarButtonsToRemove: ['autoScale2d', 'select2d', 'zoom2d', 'lasso2d', 'toImage', 'pan2d'],
+    modeBarButtonsToRemove: [
+      "autoScale2d",
+      "select2d",
+      "zoom2d",
+      "lasso2d",
+      "toImage",
+      "pan2d",
+    ],
     displayModeBar: true,
-    scrollZoom: true
+    scrollZoom: true,
   });
+  return datesWithNaN;
 }
