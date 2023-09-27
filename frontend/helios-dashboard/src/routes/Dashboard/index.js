7888811e90a22e1e130b28fd6d5c4b602b9e2ecc
Jack Blake <jhblake2@wisc.edu>
2023-09-27 13:59:10 -0500

    make ommited dates accordian

--- frontend/helios-dashboard/src/routes/Dashboard/index.js
+++ frontend/helios-dashboard/src/routes/Dashboard/index.js
@@ -1,5 +1,6 @@
-import { Box, MenuItem, Select, Stack, Typography } from "@mui/material";
+import { Box, MenuItem, Select, Stack, Typography, Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 import { useEffect, useState } from "react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import Divider from "@mui/material/Divider";
 import FILTER_REMAPPINGS from "../../config/filterRemappings.js";
@@ -7,7 +8,6 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import Page from "../../layouts/Page";
-import Paper from "@mui/material/Paper";
 import SavingsBanner from "./SavingsBanner";
 import { aggregateOutputData } from "./FetchData";
 import { createLinePlot } from "./Lineplot";
@@ -20,6 +20,7 @@ export default function Dashboard() {
   const [selectedIds, setSelectedIds] = useState([]);
   const [selectedName, setSelectedName] = useState("all");
   const [datesWithNaN, setDatesWithNaN] = useState(new Set());
+
 
   useEffect(() => {
     aggregateOutputData().then((data) => {
@@ -67,53 +68,59 @@ export default function Dashboard() {
   };
 
   return (
-    <Page title="Dashboard">
-      <Stack direction="row" sx={{ alignItems: "center", mb: 2 }}>
-        <Typography htmlFor="solar-array-select" sx={{ mr: 2 }}>
-          Select Solar Array:
-        </Typography>
-        <Select
-          id="solar-array-select"
-          value={selectedName}
-          onChange={handleChange}
-        >
-          <MenuItem value="all" selected>
-            All
-          </MenuItem>
-          {panelNames &&
-            panelNames.map((name) => (
-              <MenuItem key={name} value={name}>
-                {name}
-              </MenuItem>
-            ))}
-        </Select>
-      </Stack>
-      <SavingsBanner
-        name={selectedName}
-        data={data}
-        selectedIds={selectedIds}
-      />
-      <Box
-        id="plot-container-percent"
-        sx={{ width: "100%", height: "400px" }}
-      ></Box>
-      <Box id="plot-container" sx={{ width: "100%", height: "400px" }}></Box>
-      <Box id="precip-container" sx={{ width: "100%", height: "400px" }}></Box>
-      <Paper sx={{ mt: 4, mb: 1, p: 1 }}>
-        <Typography variant="h6" sx={{ mb: 0 }}>
-          Omitted Dates (due to data collection errors):
-        </Typography>
-        <List>
-          {[...datesWithNaN].map((date, index) => (
-            <>
-              <ListItem key={index}>
-                <ListItemText primary={new Date(date).toLocaleDateString()} />
-              </ListItem>
-              {index !== datesWithNaN.size - 1 && <Divider />}
-            </>
-          ))}
-        </List>
-      </Paper>
-    </Page>
+    <>
+      <Page title="Dashboard">
+        <Stack direction="row" sx={{ alignItems: "center", mb: 2 }}>
+          <Typography htmlFor="solar-array-select" sx={{ mr: 2 }}>
+            Select Solar Array:
+          </Typography>
+          <Select
+            id="solar-array-select"
+            value={selectedName}
+            onChange={handleChange}
+          >
+            <MenuItem value="all" selected>
+              All
+            </MenuItem>
+            {panelNames &&
+              panelNames.map((name) => (
+                <MenuItem key={name} value={name}>
+                  {name}
+                </MenuItem>
+              ))}
+          </Select>
+        </Stack>
+        <SavingsBanner
+          name={selectedName}
+          data={data}
+          selectedIds={selectedIds}
+        />
+        <Box
+          id="plot-container-percent"
+          sx={{ width: "100%", height: "400px" }}
+        ></Box>
+        <Box id="plot-container" sx={{ width: "100%", height: "400px" }}></Box>
+        <Box id="precip-container" sx={{ width: "100%", height: "400px" }}></Box>
+        <Accordion defaultExpanded={false} sx={{ mt: 4, p: 0.5 }}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="h6" sx={{ mb: 0 }}>
+              Omitted Dates (due to data collection errors)
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <List>
+              {[...datesWithNaN].map((date, index) => (
+                <>
+                  <ListItem key={index}>
+                    <ListItemText primary={new Date(date).toLocaleDateString()} />
+                  </ListItem>
+                  {index !== datesWithNaN.size - 1 && <Divider />}
+                </>
+              ))}
+            </List>
+          </AccordionDetails>
+        </Accordion>
+      </Page>
+    </>
   );
 }
